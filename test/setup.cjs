8009644af16abbd25e69c4e1dcca859f804dd9f4
JamilServices <jamilservicos@gmail.com>
2024-02-07 17:12:49 -0300

    feat: Add test setup file for commonJS modules
    Add a new test setup file for commonJS modules. The file includes the necessary imports and configurations for running tests on commonJS files. It also includes a mock module for testing purposes. The file also executes the tests for each commonJS file and logs any errors encountered during the execution. Finally, it requires the plain.cjs build file.

---
+++ test/setup.cjs
@@ -0,0 +1,22 @@
+"use strict";
+
+const {resolve} = require("node:path");
+const fastGlob = require('fast-glob');
+const {globSync} = fastGlob;
+
+const sourceFiles = resolve('src');
+const buildFiles = resolve('build');
+const mock = require("./mock.cjs");
+
+const commonJsfiles = globSync(sourceFiles +"/**/*.spec.cjs");
+
+if(commonJsfiles.length >= 1) {
+    commonJsfiles.forEach(file => {
+        try {
+            require(file)(mock);
+        } catch {
+            console.log("error on:", file);
+        }
+    });
+}
+require("./build/plain.cjs");
